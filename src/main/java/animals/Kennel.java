a4248ba23f0b300ac7d479e733fc12723327b292
nick.kontsevych@gmail.com <nick.kontsevych@gmail.com>
2019-08-06 21:50:13 +0300

    * added kennel

--- src/main/java/animals/Kennel.java
+++ src/main/java/animals/Kennel.java
@@ -4,7 +4,35 @@ import java.util.ArrayList;
 
 public class Kennel {
 
-    ArrayList<Object> kennel = new ArrayList<Object>();
+    ArrayList<Object> animal = new ArrayList<Object>();
 
+    Dog dog = new Dog(10);
+    Dog dog2 = new Dog(10);
+    Dog dog3 = new Dog(10);
+    Cat cat = new Cat(10);
+
+    
+
+    public void getAnimal() {
+        animal.add(dog);
+        animal.add(dog2);
+        animal.add(dog3);
+        animal.add(cat);
+
+        for(Object i:animal ){
+            if (i.getClass() == dog.getClass()){
+
+            }
+        }
+        System.out.println(animal.get(1).getClass());
+    }
+
+    int size = animal.size();
+
+    public static void main(String[] args) {
+        Kennel kennel = new Kennel();
+
+        kennel.calsss();
+    }
 
 }
