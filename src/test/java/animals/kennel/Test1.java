08d49192de3bc9b9372be76d658fb57e0dc3b20a
mkontsevych <nick.kontsevych@gmail.com>
2019-08-07 13:10:42 +0300

    * added fist test probably does not work

--- src/test/java/animals/kennel/Test1.java
+++ src/test/java/animals/kennel/Test1.java
@@ -1,4 +1,41 @@
 package animals.kennel;
 
+import animals.Cat;
+import animals.Dog;
+import animals.Kennel;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
 public class Test1 {
+
+    @Test
+    public void testAddAnimal() {
+        Dog dog = new Dog(10, "Doom");
+        Cat cat = new Cat(5, "Sony");
+        Dog dog1 = new Dog(10, "Doom1");
+        Cat cat1 = new Cat(5, "Sony1");
+        Dog dog2 = new Dog(10, "Doom2");
+        Cat cat2 = new Cat(5, "Sony2");
+
+        Kennel kennel = new Kennel();
+        ArrayList<Object> testKennel = new ArrayList<Object>();
+
+        testKennel.add(dog);
+        testKennel.add(dog1);
+        testKennel.add(dog2);
+        testKennel.add(cat);
+        testKennel.add(cat1);
+        testKennel.add(cat2);
+
+        kennel.addAnimal(dog);
+        kennel.addAnimal(dog1);
+        kennel.addAnimal(dog2);
+        kennel.addAnimal(cat);
+        kennel.addAnimal(cat1);
+        kennel.addAnimal(cat2);
+
+        
+    }
 }
