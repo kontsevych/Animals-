5c70651b9af0946c8998b0908f6b2243370e9771
nick.kontsevych@gmail.com <nick.kontsevych@gmail.com>
2019-08-09 19:06:20 +0300

    * added kennel

--- src/main/java/animals/Kennel.java
+++ src/main/java/animals/Kennel.java
@@ -11,14 +11,14 @@ public class Kennel {
     Dog dog3 = new Dog(10);
     Cat cat = new Cat(10);
 
-    
+
 
     public void getAnimal() {
         animal.add(dog);
         animal.add(dog2);
         animal.add(dog3);
         animal.add(cat);
-
+//Додати ще два лісти
         for(Object i:animal ){
             if (i.getClass() == dog.getClass()){
 
