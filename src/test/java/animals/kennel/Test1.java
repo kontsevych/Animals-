b5668c69984acb8ab94c0ba0b4ee775ae0eed648
mkontsevych <nick.kontsevych@gmail.com>
2019-09-19 09:38:35 +0300

    Refactoring:added animals qnt verification

--- src/test/java/animals/kennel/Test1.java
+++ src/test/java/animals/kennel/Test1.java
@@ -12,7 +12,7 @@ import java.io.InvalidObjectException;
 public class Test1 extends Animal {
 
     @Test
-    public void testGetDog() throws  java.lang.NullPointerException {
+    public void testGetDog() throws java.lang.NullPointerException {
         Kennel kennel = new Kennel();
 
         Dog[] dog = new Dog[10];
@@ -104,32 +104,11 @@ public class Test1 extends Animal {
             kennel.addAnimal(cat[counter]);
         }
 
-        for (Object petsContainer : kennel.getAllPets()) {
-            result = petsContainer.getClass() == Cat.class || petsContainer.getClass() == Dog.class;
+        if (dog.length + cat.length == kennel.getAllPets().size()) {
+            for (Animal petsContainer : kennel.getAllPets()) {
+                result = petsContainer.getClass() == Cat.class || petsContainer.getClass() == Dog.class;
+            }
         }
         Assert.assertTrue(result);
     }
-
-    @Test
-    public void testGetAllAnimals() throws NullPointerException {
-        Kennel kennel = new Kennel();
-        boolean result = false;
-
-        Dog[] dog = new Dog[10];
-        Cat[] cat = new Cat[10];
-
-        for (int counter = 0; counter < 10; counter++) {
-            dog[counter] = new Dog(10, "Dog");
-            cat[counter] = new Cat(10, "Cat");
-            kennel.addAnimal(dog[counter]);
-            kennel.addAnimal(cat[counter]);
-        }
-
-        for (Object petsContainer : kennel.getAnimals(Dog.class)) {
-            System.out.println(petsContainer.toString());
-            result = petsContainer.getClass() == Cat.class || petsContainer.getClass() == Dog.class;
-        }
-        Assert.assertTrue(result);
-    }
-
 }
